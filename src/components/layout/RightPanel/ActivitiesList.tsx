71bb6d3897a7e6685cf24a9b27d583bb2bbe8706
Shivam <shivambhushanedu@gmail.com>
2025-09-19 03:06:12 +0530

    acvities done

--- src/components/layout/RightPanel/ActivitiesList.tsx
+++ src/components/layout/RightPanel/ActivitiesList.tsx
@@ -3,26 +3,31 @@ import { mockActivities } from '../../../data';
 
 interface ActivityItemProps {
   activity: Activity;
+  isLast: boolean;
 }
 
-function ActivityItem({ activity }: ActivityItemProps) {
+function ActivityItem({ activity, isLast }: ActivityItemProps) {
   return (
     <div className="flex gap-2 p-1 rounded-lg">
-      {/* Avatar */}
-      <div className="flex items-center justify-center flex-shrink-0">
+      {/* Avatar with Timeline */}
+      <div className="flex justify-center flex-shrink-0 relative">
         <img 
           src={activity.avatar} 
           alt="" 
-          className="w-6 h-6 rounded-full object-cover" 
+          className="w-6 h-6 rounded-full object-cover relative z-10" 
         />
+        {/* Timeline connector - short segment with gaps before and after avatars */}
+        {!isLast && (
+          <div className="absolute top-8 left-1/2 transform -translate-x-1/2 w-px h-3.5 bg-[rgba(28,28,28,0.1)]"></div>
+        )}
       </div>
       
       {/* Content */}
-      <div className="flex flex-col justify-center flex-1">
-        <p className="text-sm font-normal text-[#1C1C1C] leading-tight">
+      <div className="flex flex-col flex-1 min-w-0">
+        <p className="text-sm font-normal text-[#1C1C1C] leading-[1.4285714285714286] truncate">
           {activity.title}
         </p>
-        <p className="text-xs font-normal text-[rgba(28,28,28,0.4)] leading-tight">
+        <p className="text-xs font-normal text-[rgba(28,28,28,0.4)] leading-[1.5] truncate">
           {activity.time}
         </p>
       </div>
@@ -33,28 +38,21 @@ function ActivityItem({ activity }: ActivityItemProps) {
 export default function ActivitiesList() {
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 self-stretch">
       {/* Title */}
-      <div className="px-1 py-2">
-        <h3 className="text-sm font-semibold text-[#1C1C1C]">Activities</h3>
+      <div className="px-2 py-1 self-stretch">
+        <h3 className="text-sm font-semibold text-[#1C1C1C] leading-[1.4285714285714286]">Activities</h3>
       </div>
       
       {/* Activities Items */}
-      <div className="relative flex flex-col gap-2">
-        {mockActivities.map((activity) => (
+      <div className="flex flex-col gap-2">
+        {mockActivities.map((activity, index) => (
           <ActivityItem 
             key={activity.id} 
             activity={activity} 
+            isLast={index === mockActivities.length - 1}
           />
         ))}
-        
-        {/* Timeline connector - vertical line with dots */}
-        <div className="absolute left-[16px] top-[80px] w-[1px] h-[176px] bg-[rgba(28,28,28,0.1)]">
-          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-1 h-1 bg-[rgba(28,28,28,0.1)] rounded-full"></div>
-          <div className="absolute top-[40px] left-1/2 transform -translate-x-1/2 w-1 h-1 bg-[rgba(28,28,28,0.1)] rounded-full"></div>
-          <div className="absolute top-[80px] left-1/2 transform -translate-x-1/2 w-1 h-1 bg-[rgba(28,28,28,0.1)] rounded-full"></div>
-          <div className="absolute top-[120px] left-1/2 transform -translate-x-1/2 w-1 h-1 bg-[rgba(28,28,28,0.1)] rounded-full"></div>
-        </div>
       </div>
     </div>
   );
